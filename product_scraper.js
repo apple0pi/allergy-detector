1d8c7aa4054c822ed0ea271e7e032d0b8226cdbb
Angela Ngo <54916204+apple0pi@users.noreply.github.com>
2024-08-01 17:55:55 -0400

    it can properly navigate to a page now

--- product_scraper.js
+++ product_scraper.js
@@ -1,5 +1,5 @@
 import inquirer from "inquirer";
-
+import puppeteer  from "puppeteer";
 const baseURL = 'https://cosdna.com/eng/product.php?q='; 
 let url = baseURL; 
 let productName; 
@@ -30,29 +30,50 @@ async function createURL(){
     // add the last part of the product name  
     url+=((brokenUpName[brokenUpName.length-1]) + "&sort=featured"); 
 };
-// async function createURL(){ // make async
-//     let brokenUpName = productName.split(" "); 
-//     for (let i = 0; i < brokenUpName.length-1; i++){ // dont include the last one 
-//         param = brokenUpName[i]; 
-//         if(param.includes("%")) {
-//             url+=(param + "25+");
-//         }else if(param.includes("'")){
-//             param = param.replace("'","%27"); // replacing the apostrophe with this symbol instead 
-//             url+= (param + "+");
-//         }else{
-//             url+=(param + "+"); 
-//         }
-//     }
 
-//     // add the last part of the product name  
-//     url+=((brokenUpName[brokenUpName.length-1]) + "&sort=featured"); 
-//     return url; 
-// }
+async function scrape(){
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage(); 
+
+    console.log(url)
+    console.log(`Navigating to: ${url}`);
+
+    try {
+        await page.goto(url);
+    } catch (error) {
+        console.error(`Failed to navigate to ${url}:`, error);
+        await browser.close();
+        return;
+    }
+
+    await page.waitForSelector('a.inline-block.w-full');
+
+    // Select the first <a> element with the specified class
+    const link = await page.$('a.inline-block.w-full');
+
+    // Ensure the element has an href attribute
+    const hasHref = await page.evaluate(el => el.hasAttribute('href'), link);
+
+    if (hasHref) {
+        // Optionally, click the link if desired
+        await link.click();
+        
+        // Optionally, wait for navigation if needed
+        await page.waitForNavigation();
+    } else {
+        console.log('No link with the specified class and href found.');
+    }
+
+    await browser.close(); // Close the browser when done
+
+
+}; 
 
 async function main(){
     await getProductInput(); 
     await createURL(); 
-    console.log(url);
+    console.log(url)
+    await scrape().catch(console.error);
 }
 
 main()
